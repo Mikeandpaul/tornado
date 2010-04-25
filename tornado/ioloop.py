6f477b682a57707fbd269cdd5a2e5da05cd9c46b
Paul Böhm <paul@boehm.org>
2010-04-25 01:05:52 -0700

    revert

--- tornado/ioloop.py
+++ tornado/ioloop.py
@@ -197,7 +197,7 @@ class IOLoop(object):
             try:
                 event_pairs = self._impl.poll(poll_timeout)
             except Exception, e:
-                if e.errno == errno.EINTR:
+                if hasattr(e, 'errno') and e.errno == errno.EINTR:
                     _log.warning("Interrupted system call", exc_info=1)
                     continue
                 else:
@@ -376,6 +376,49 @@ class _EPoll(object):
     def poll(self, timeout):
         return epoll.epoll_wait(self._epoll_fd, int(timeout * 1000))
 
+class _LibEvent(object):
+    """A libevent based IOLoop implementation"""
+
+    def __init__(self):
+        self._eb = libevent.EventBase()
+        self._events = {}
+        self._ready_fds = {}
+
+    def _fd_ready(self, fd, events, eventObj):
+        eventmask = (IOLoop.READ * (events & libevent.EV_READ) / libevent.EV_READ) | \
+                    (IOLoop.WRITE * (events & libevent.EV_WRITE) / libevent.EV_WRITE) | \
+                    (IOLoop.ERROR * (events & libevent.EV_TIMEOUT) / libevent.EV_TIMEOUT)
+
+        self._ready_fds[fd] = eventmask
+
+    def register(self, fd, events):
+        eventmask = 0
+        if events & IOLoop.READ:
+            eventmask = eventmask | libevent.EV_READ | libevent.EV_PERSIST
+        if events & IOLoop.WRITE:
+            eventmask = eventmask | libevent.EV_WRITE
+        if events & IOLoop.ERROR:
+            # does libevent has error event type?
+            eventmask = eventmask | libevent.EV_TIMEOUT
+
+        self._events[fd] = self._eb.create_event(fd, eventmask, self._fd_ready)
+        self._events[fd].add_to_loop()
+
+    def modify(self, fd, events):
+        self.unregister(fd)
+        self.register(fd, events)
+
+    def unregister(self, fd):
+        self._events[fd].remove_from_loop()
+        del self._events[fd]
+
+    def poll(self, timeout=10):
+        self._ready_fds = {}
+        self._eb.loop_exit(timeout)
+        # if libevent.EVLOOP_NONBLOCK is used, the process eats up the cpu
+        self._eb.loop(libevent.EVLOOP_ONCE)
+        return self._ready_fds.items()
+
 
 class _KQueue(object):
     """A kqueue-based event loop for BSD/Mac systems."""
@@ -461,23 +504,67 @@ class _Select(object):
             events[fd] = events.get(fd, 0) | IOLoop.ERROR
         return events.items()
 
+class _ZMQ(object):
+    """Integrate with the ZMQ Event loop. PyZMQ already exposes a poll-like interface.
+       This piece of crap code just translates from poll to epoll."""
+    def __init__(self):
+        self.poller = zmq.Poller()
+        
+    def register(self, fd, events):
+        flags = 0
+        if events & IOLoop.READ: flags |= zmq.POLLIN
+        if events & IOLoop.WRITE: flags |= zmq.POLLOUT
+        if events & IOLoop.ERROR: flags |= zmq.POLLERR
+        
+        self.poller.register(fd, flags)
+
+    def modify(self, fd, events):
+        self.register(fd, events)
+
+    def unregister(self, fd):
+        self.poller.unregister(fd)
+
+    def poll(self, timeout):
+        if timeout is None:
+            timeout = -1
+        else:
+            timeout *= 1000
+        results = self.poller.poll(timeout)
+        reslist = []
+        for fd, flags in results:
+            outflags = 0
+            if flags & zmq.POLLIN: outflags |= IOLoop.READ
+            if flags & zmq.POLLOUT: outflags |= IOLoop.WRITE
+            if flags & zmq.POLLERR: outflags |= IOLoop.ERROR
+            reslist.append ( (fd, outflags) )
+        return reslist
 
 # Choose a poll implementation. Use epoll if it is available, fall back to
 # select() for non-Linux platforms
-if hasattr(select, "epoll"):
-    # Python 2.6+ on Linux
-    _poll = select.epoll
-elif hasattr(select, "kqueue"):
-    # Python 2.6+ on BSD or Mac
-    _poll = _KQueue
-else:
-    try:
-        # Linux systems with our C module installed
-        import epoll
-        _poll = _EPoll
-    except:
-        # All other systems
-        import sys
-        if "linux" in sys.platform:
-            _log.warning("epoll module not found; using select()")
-        _poll = _Select
+
+try:
+    import zmq
+    _poll = _ZMQ
+except:
+    if hasattr(select, "epoll"):
+        # Python 2.6+ on Linux
+        _poll = select.epoll
+    elif hasattr(select, "kqueue"):
+        # Python 2.6+ on BSD or Mac
+        _poll = _KQueue
+    else:
+        try:
+            # try to use libevent
+            import libevent
+            _poll = _LibEvent
+        except:
+            try:
+                # Linux systems with our C module installed
+                import epoll
+                _poll = _EPoll
+            except:
+                # All other systems
+                import sys
+                if "linux" in sys.platform:
+                    _log.warning("epoll module not found; using select()")
+                _poll = _Select
